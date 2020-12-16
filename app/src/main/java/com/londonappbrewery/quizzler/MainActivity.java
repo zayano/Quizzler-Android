16b6643d87fa7ce5e29e27fd712cafb3a2b8be89
Reza Aditya <adityareza46@gmail.com>
2020-12-16 16:54:40 +0700

    Retrieve Quiz Questions from an Array and Display them on Screen

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
@@ -18,23 +19,28 @@ public class MainActivity extends Activity {
     // TODO: Declare member variables here:
     Button mTrueButton;
     Button mFalseButton;
+    TextView mQuestionTextView;
+    int mIndex;
+    int mQuestion;
 
     // TODO: Uncomment to create question bank
-//    private TrueFalse[] mQuestionBank = new TrueFalse[] {
-//            new TrueFalse(R.string.question_1, true),
-//            new TrueFalse(R.string.question_2, true),
-//            new TrueFalse(R.string.question_3, true),
-//            new TrueFalse(R.string.question_4, true),
-//            new TrueFalse(R.string.question_5, true),
-//            new TrueFalse(R.string.question_6, false),
-//            new TrueFalse(R.string.question_7, true),
-//            new TrueFalse(R.string.question_8, false),
-//            new TrueFalse(R.string.question_9, true),
-//            new TrueFalse(R.string.question_10, true),
-//            new TrueFalse(R.string.question_11, false),
-//            new TrueFalse(R.string.question_12, false),
-//            new TrueFalse(R.string.question_13,true)
-//    };
+    private TrueFalse[] mQuestionBank = new TrueFalse[]{
+            new TrueFalse(R.string.question_1, true),
+            new TrueFalse(R.string.question_2, true),
+            new TrueFalse(R.string.question_3, true),
+            new TrueFalse(R.string.question_4, true),
+            new TrueFalse(R.string.question_5, true),
+            new TrueFalse(R.string.question_6, false),
+            new TrueFalse(R.string.question_7, true),
+            new TrueFalse(R.string.question_8, false),
+            new TrueFalse(R.string.question_9, true),
+            new TrueFalse(R.string.question_10, true),
+            new TrueFalse(R.string.question_11, false),
+            new TrueFalse(R.string.question_12, false),
+            new TrueFalse(R.string.question_13, true)
+    };
+
+    int[] myIntArray = new int[]{2, 4, 6};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,25 +49,28 @@ public class MainActivity extends Activity {
 
         mTrueButton = findViewById(R.id.true_button);
         mFalseButton = findViewById(R.id.false_button);
+        mQuestionTextView = findViewById(R.id.question_text_view);
 
-        View.OnClickListener myListener = new View.OnClickListener() {
+        mQuestionTextView.setText("Birds are Dinosaurs");
+
+        mQuestion = mQuestionBank[mIndex].getmQuestionID();
+        mQuestionTextView.setText(mQuestion);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("Quizzler", "Button Pressed!");
                 Toast.makeText(getApplicationContext(), "True Pressed!", Toast.LENGTH_SHORT).show();
             }
-        };
-
-        mTrueButton.setOnClickListener(myListener);
+        });
 
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast myToast =  Toast.makeText(getApplicationContext(), "False Pressed!", Toast.LENGTH_SHORT);
+                Toast myToast = Toast.makeText(getApplicationContext(), "False Pressed!", Toast.LENGTH_SHORT);
                 myToast.show();
             }
         });
-
 
 
     }
